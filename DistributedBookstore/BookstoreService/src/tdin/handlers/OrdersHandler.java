cfbef7c35899b2af87d8d979c911d5a028f565ce
João Silva <joaosilva@revtut.net>
2017-05-15 15:33:30 +0100

    Fixed methods to get orders.

--- DistributedBookstore/BookstoreService/src/tdin/handlers/OrdersHandler.java
+++ DistributedBookstore/BookstoreService/src/tdin/handlers/OrdersHandler.java
@@ -27,34 +27,49 @@ public class OrdersHandler {
 
     }
 
+    private BookOrder parseBookOrder(ResultSet result) throws SQLException {
+        BookOrder bookOrder = new BookOrder(
+                UUID.fromString(result.getString(BookOrder.ORDER_ID_COLUMN)),
+                result.getString(BookOrder.BOOK_TITLE_COLUMN),
+                result.getInt(BookOrder.QUANTITY_COLUMN),
+                result.getString(BookOrder.CLIENT_NAME_COLUMN),
+                result.getString(BookOrder.CLIENT_ADDRESS_COLUMN),
+                result.getString(BookOrder.CLIENT_EMAIL_COLUMN)
+        );
+        BookOrder.State state = BookOrder.State.values()[result.getInt(BookOrder.STATE_COLUMN)];
+        switch (state) {
+            case DISPATCHED:
+                bookOrder.dispatched(new Date(result.getLong(BookOrder.STATE_DATE_COLUMN)));
+                break;
+            case WILL_DISPATCH:
+                bookOrder.willDispatch(new Date(result.getLong(BookOrder.STATE_DATE_COLUMN)));
+                break;
+        }
+
+        return bookOrder;
+    }
+
+    public BookOrder getBookOrder(UUID id) throws SQLException {
+        ResultSet result = DatabaseAPI.executeQuery(Core.getInstance().getDatabase(),
+                "store_orders",
+                Collections.singletonList("*"),
+                Collections.singletonList("id"),
+                Collections.<Object>singletonList(id.toString()));
+
+        if(!result.next())
+            return null;
+
+        return parseBookOrder(result);
+    }
+
     public BookOrderList getBookOrders() throws SQLException {
         ResultSet result = DatabaseAPI.executeQuery(Core.getInstance().getDatabase(),
                 "store_orders",
                 Collections.singletonList("*"));
 
         List<BookOrder> bookOrders = new ArrayList<>();
-        BookOrder bookOrder;
         while (result.next()) {
-            bookOrder = new BookOrder(
-                    UUID.fromString(result.getString(BookOrder.ORDER_ID_COLUMN)),
-                    result.getString(BookOrder.BOOK_TITLE_COLUMN),
-                    result.getInt(BookOrder.QUANTITY_COLUMN),
-                    result.getString(BookOrder.CLIENT_NAME_COLUMN),
-                    result.getString(BookOrder.CLIENT_ADDRESS_COLUMN),
-                    result.getString(BookOrder.CLIENT_EMAIL_COLUMN)
-            );
-
-            BookOrder.State state = BookOrder.State.values()[result.getInt(BookOrder.STATE_COLUMN)];
-            switch (state) {
-                case DISPATCHED:
-                    bookOrder.dispatched(new Date(result.getLong(BookOrder.STATE_DATE_COLUMN)));
-                    break;
-                case WILL_DISPATCH:
-                    bookOrder.willDispatch(new Date(result.getLong(BookOrder.STATE_DATE_COLUMN)));
-                    break;
-            }
-
-            bookOrders.add(bookOrder);
+            bookOrders.add(parseBookOrder(result));
         }
 
         return new BookOrderList(bookOrders);
--- DistributedBookstore/BookstoreService/src/tdin/services/Orders.java
+++ DistributedBookstore/BookstoreService/src/tdin/services/Orders.java
@@ -5,7 +5,9 @@ import tdin.handlers.OrdersHandler;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.UUID;
 
 @Path("orders")
 public class Orders {
@@ -13,8 +15,19 @@ public class Orders {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
-    public String getOrder(@PathParam("id") String id) {
-        return new BookOrder("O livro", 1, "O cliente", "O endereço", "O email").toJSON().toString();
+    public Response getOrder(@PathParam("id") String id) throws SQLException {
+        UUID uuid;
+        try {
+            uuid = UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        BookOrder bookOrder = OrdersHandler.getInstance().getBookOrder(uuid);
+        if(bookOrder == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(bookOrder.toJSON().toString()).build();
     }
 
     @GET
