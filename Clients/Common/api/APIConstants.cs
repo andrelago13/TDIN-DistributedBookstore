1e9bd02f1ebc0b6c5aa210bfddf8b616ba74351a
André Lago <andrelago1995@gmail.com>
2017-05-15 22:28:42 +0100

    Constants

---
+++ Clients/Common/api/APIConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.api
+{
+    public class APIConstants
+    {
+        public static readonly string BASE_URL = "http://localhost:8080/";
+
+        public static readonly string WAREHOUSE_PATH = BASE_URL + "warehouse/";
+        public static readonly string WAREHOUSE_ORDERS_PATH = WAREHOUSE_PATH + "orders/";
+        public static readonly string WAREHOUSE_ORDERS_UPDATE_PATH = WAREHOUSE_PATH + WAREHOUSE_ORDERS_PATH + "update/";
+    }
+}
