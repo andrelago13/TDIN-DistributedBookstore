263e7f226cdec320e9d54cdd4596399cf318553b
João Silva <joaosilva@revtut.net>
2017-05-15 16:00:09 +0100

    Added random data for stock.

---
+++ DistributedBookstore/Common/database/stock.sql
@@ -0,0 +1,31 @@
+drop table if exists stock;
+create table stock (
+  book_id INT UNIQUE,
+  quantity INT
+);
+
+insert into stock (book_id, quantity) values (28, 45);
+insert into stock (book_id, quantity) values (16, 13);
+insert into stock (book_id, quantity) values (10, 97);
+insert into stock (book_id, quantity) values (42, 91);
+insert into stock (book_id, quantity) values (41, 82);
+insert into stock (book_id, quantity) values (38, 45);
+insert into stock (book_id, quantity) values (5, 75);
+insert into stock (book_id, quantity) values (20, 92);
+insert into stock (book_id, quantity) values (31, 76);
+insert into stock (book_id, quantity) values (47, 21);
+insert into stock (book_id, quantity) values (49, 35);
+insert into stock (book_id, quantity) values (18, 57);
+insert into stock (book_id, quantity) values (13, 13);
+insert into stock (book_id, quantity) values (44, 12);
+insert into stock (book_id, quantity) values (37, 50);
+insert into stock (book_id, quantity) values (6, 88);
+insert into stock (book_id, quantity) values (22, 71);
+insert into stock (book_id, quantity) values (7, 13);
+insert into stock (book_id, quantity) values (9, 95);
+insert into stock (book_id, quantity) values (2, 15);
+insert into stock (book_id, quantity) values (27, 76);
+insert into stock (book_id, quantity) values (19, 66);
+insert into stock (book_id, quantity) values (48, 86);
+insert into stock (book_id, quantity) values (50, 26);
+insert into stock (book_id, quantity) values (30, 27);
