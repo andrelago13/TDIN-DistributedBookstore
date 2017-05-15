ebce71b355abab3df44c7872328045fbfc399821
João Silva <joaosilva@revtut.net>
2017-05-15 23:00:16 +0100

    Updated stock random data.

--- DistributedBookstore/Common/database/stock.sql
+++ DistributedBookstore/Common/database/stock.sql
@@ -1,31 +1,30 @@
 drop table if exists stock;
 create table stock (
-  book_id INT PRIMARY KEY,
+  book_id VARCHAR(50),
   quantity INT
 );
-
-insert into stock (book_id, quantity) values (28, 45);
-insert into stock (book_id, quantity) values (16, 13);
-insert into stock (book_id, quantity) values (10, 97);
-insert into stock (book_id, quantity) values (42, 91);
-insert into stock (book_id, quantity) values (41, 82);
-insert into stock (book_id, quantity) values (38, 45);
-insert into stock (book_id, quantity) values (5, 75);
-insert into stock (book_id, quantity) values (20, 92);
-insert into stock (book_id, quantity) values (31, 76);
-insert into stock (book_id, quantity) values (47, 21);
-insert into stock (book_id, quantity) values (49, 35);
-insert into stock (book_id, quantity) values (18, 57);
-insert into stock (book_id, quantity) values (13, 13);
-insert into stock (book_id, quantity) values (44, 12);
-insert into stock (book_id, quantity) values (37, 50);
-insert into stock (book_id, quantity) values (6, 88);
-insert into stock (book_id, quantity) values (22, 71);
-insert into stock (book_id, quantity) values (7, 13);
+insert into stock (book_id, quantity) values (1, 10);
+insert into stock (book_id, quantity) values (2, 36);
+insert into stock (book_id, quantity) values (3, 2);
+insert into stock (book_id, quantity) values (4, 56);
+insert into stock (book_id, quantity) values (5, 25);
+insert into stock (book_id, quantity) values (6, 25);
+insert into stock (book_id, quantity) values (7, 17);
+insert into stock (book_id, quantity) values (8, 82);
 insert into stock (book_id, quantity) values (9, 95);
-insert into stock (book_id, quantity) values (2, 15);
-insert into stock (book_id, quantity) values (27, 76);
-insert into stock (book_id, quantity) values (19, 66);
-insert into stock (book_id, quantity) values (48, 86);
-insert into stock (book_id, quantity) values (50, 26);
-insert into stock (book_id, quantity) values (30, 27);
+insert into stock (book_id, quantity) values (10, 68);
+insert into stock (book_id, quantity) values (11, 49);
+insert into stock (book_id, quantity) values (12, 8);
+insert into stock (book_id, quantity) values (13, 87);
+insert into stock (book_id, quantity) values (14, 72);
+insert into stock (book_id, quantity) values (15, 17);
+insert into stock (book_id, quantity) values (16, 62);
+insert into stock (book_id, quantity) values (17, 13);
+insert into stock (book_id, quantity) values (18, 3);
+insert into stock (book_id, quantity) values (19, 86);
+insert into stock (book_id, quantity) values (20, 61);
+insert into stock (book_id, quantity) values (21, 100);
+insert into stock (book_id, quantity) values (22, 33);
+insert into stock (book_id, quantity) values (23, 14);
+insert into stock (book_id, quantity) values (24, 9);
+insert into stock (book_id, quantity) values (25, 31);
