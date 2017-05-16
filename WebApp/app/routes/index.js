c83dc9ddc5272b0b194c3d06cc1fc65f439713ea
André Lago <andrelago1995@gmail.com>
2017-05-16 12:35:17 +0100

    Homepage

--- WebApp/app/routes/index.js
+++ WebApp/app/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var config = require('./../configuration/config');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: config.app_title
+  });
 });
 
 module.exports = router;
