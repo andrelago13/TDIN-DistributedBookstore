1e031820e0241161800863cab15f2a04ea09369d
André Lago <andrelago1995@gmail.com>
2017-05-22 13:59:03 +0100

    Register fix

--- DistributedBookstore/WarehouseService/res/warehouse.sql
+++ DistributedBookstore/WarehouseService/res/warehouse.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS warehouse_orders;
-DROP TABLE IF EXISTS users;
 
 CREATE TABLE warehouse_orders (
   id             VARCHAR(40) PRIMARY KEY,
@@ -11,17 +10,9 @@ CREATE TABLE warehouse_orders (
   dispatch_date  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE users (
-  id       INTEGER PRIMARY KEY,
-  username VARCHAR(60) NOT NULL UNIQUE,
-  password VARCHAR(60) NOT NULL
-);
-
 -- DUMMY DATA
 
 INSERT INTO warehouse_orders (id, quantity, book_id, state)
 VALUES ('3c759575-7d2f-495d-93c8-6c3e04418eb0', 10, 1, 0);
 INSERT INTO warehouse_orders (id, quantity, book_id, state, order_date)
 VALUES ('cfe42f04-71e3-48b1-bb02-ce2b283c0aa9', 15, 2, 2, CURRENT_TIMESTAMP);
-
-INSERT INTO users (id, username, password) VALUES (1, 'USER', '123456');
